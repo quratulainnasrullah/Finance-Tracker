ab468814b82653c8a9e0bd31ebbbfd7427799c88
Qurat ul Ain <quratulainnasrullah@gmail.com>
2022-09-08 14:35:50 +0500

    add devise gem and create users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+   
+    def after_sign_in_path_for(resource)
+        
+        root_path
+    end
+    
+
 end
